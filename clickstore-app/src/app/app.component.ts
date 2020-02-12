13a1226ab391d9cfbe9d744a17a090b739f9e709
legrand883 <60906970+legrand883@users.noreply.github.com>
2020-02-11 20:38:48 -0600

    FUNCIONES Y EJERCICIO

--- clickstore-app/src/app/app.component.ts
+++ clickstore-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { collectExternalReferences } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,7 @@ export class AppComponent  implements OnInit {
   title = 'clickstore-app';
 
   ngOnInit()
-{
+  {
 
   let nombre 		     = "Emmanuel";
   let nombre2:string = "Emmanuel 2";
@@ -17,7 +18,7 @@ export class AppComponent  implements OnInit {
 
   let booleano: boolean = true;
   let nombre4:  any     = 'Emmanuel 4'
-  let nombre5:  string | number = 3;
+  let nombre5:  string  | number = 3;
 
   let objeto1: any = {
   	  nombre  : "Emmanuel",
@@ -57,8 +58,132 @@ export class AppComponent  implements OnInit {
        })
   }
   // FIN arrow functions
+
   usuario.setNombre("Lorenita");
   resultado.innerHTML = usuario.getNombre();
 
+  // --- 
+
+  for(let i=0; i<arreglo2.length; i++)
+  {
+    console.log(arreglo2[i]);
+  }
+
+// --- 
+  for(let valor of arreglo3)
+  {
+    console.log(valor);
+  }
+
+  let arreglo4: any [] = 
+  [
+    {
+      nombre:"Emmanuel",
+      apellido:"Villalobos"
+    },
+    {
+      nombre:"Luis",
+      apellido:"Hernandez"
+    }
+  ]
+
+// --- 
+  for (let obj of arreglo4)
+  {
+    console.log(obj);
+    console.log(obj.nombre);
+  }
+
+  arreglo4.forEach((item:any): void=>
+  {
+console.log(item)
+  })
+
+  //FILTER, MAP, FIND, SORT
+
+  let nombres : string[] = arreglo4.map(item=>
+    {
+      return item.nombre;
+    });
+    console.log("NOMBRES",nombres);
+
+    console.log(arreglo4[0].direccion);
+
+    if (!arreglo4[0].direccion)
+    {
+      console.log("E N T R O");
+    }
+
+    if ([].length)
+    {
+      console.log("ESTOY VACIO");
+    }
+
+    if (["L","E"].length)
+    {
+      console.log("TENGO LETRAS");
+    }
+
+
+
+    let valor1: string;
+    let valor2: number = 1;
+
+    if (valor1 === undefined)
+    {
+      console.log("NO TENGO NADA DE VALOR")
+    }
+
+
+    for (let attributo in arreglo4[0])
+    {
+      console.log("atributo",attributo);
+      console.log("valor",arreglo4[0][attributo]);
+    }
+
+    // INICIO FUNCION DE BUSQUEDA FLECHA
+    let lista_usuario: any [] = 
+  [
+    {
+      nombre:"Lorena",
+      apellido:"Jimenez"
+    },
+    {
+      nombre:"Emmanuel",
+      apellido:"Villalobos"
+    },
+    {
+      nombre:"Eduardo",
+      apellido:"Morales"
+    },
+    {
+      nombre:"Hector",
+      apellido:"Morales"
+    },
+    {
+      nombre:"Jorge",
+      apellido:"Treviño"
+    }
+  ];
+
+  let Lorena_Jimnez = lista_usuario.find(item=>
+    {
+      return item.nombre == "Lorena" && item.apellido == "Jimenez";
+    });
+
+    console.log("LORENA",Lorena_Jimnez);
+
+    // FIN FUNCION DE BUSQUEDA FLECHA
+
+    
+    
+    // E J E R C I C I O
+    let lista_numero: number[] = [1,43,3,65,3,48,9];
+    let resul: number[]=lista_numero.filter(item=>
+      {
+        return item > 40
+      })
+
+      console.log("MARYOR DE 40 SON: ",resul);
 }
 }
